ea03d805bde857a4887a406834148d88ebbda2a3
Sam Selfridge <Sam.selfridge@gmail.com>
2021-03-07 11:34:20 -0800

    Imgur handles posts different from galleries...

--- content.js
+++ content.js
@@ -1,12 +1,17 @@
 window.onload = () => {
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     let title = "";
-    try {
-      title = document.querySelector(".Gallery-Title > div.row > span").innerHTML;
-    } catch (error) {
-      title = "looks like they changed the title structure...";
+    // console.log("Get Title");
+    const titleSelectors = [".Gallery-Title > div.row > span", "h1.post-title"];
+    let i = -1;
+    while (title === "") {
+      i++;
+      try {
+        title = document.querySelector(titleSelectors[i]).innerHTML;
+      } catch (error) {}
     }
-
+    // console.log("Got Title:,", title);
+    // console.log("Last selector:", titleSelectors[i]);
     sendResponse(title);
     return true;
   });
